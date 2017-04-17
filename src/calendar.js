857063aa63684567a77b72ff1f4df3cebc17a5df
Renan Muniz <renan@renanmuniz.com>
2017-04-17 03:52:35 -0300

    Update src/main.js comments and format
    Update comments;
    Fix typos;

--- src/calendar.js
+++ src/calendar.js
@@ -1,25 +1,18 @@
-// Summary:
-//    Requirements, resources, addons and others stuffs requireds
 var colors = require('colors');
 var optionExtender = require('optionextender');
 
-// Summary:
-//   Calendar.
 // Arguments:
-//   - year(required): number of the specifed year.
-//   - month(required): number of the specified month, between(1 - 12).
-//   - day(required): number of day in the month, between(1 - 31).
+//   - year(required): number of the year.
+//   - month(required): number of the month, between 1 and 12.
+//   - day(required): number of day, between 1 and 31.
 var Calendar = function (year, month, day) {
 
-  // Summary:
-  //    Validation arguments before initalization to grant execution
-  if (typeof(year) !== 'number')  throw ('an valid `year` argument is required');
-  if (typeof(month) !== 'number') throw ('an valid `month` argument is required');
-  if (typeof(day) !== 'number')   throw ('an valid `day` argument is required');
+  // Validate the arguments.
+  if (typeof(year) !== 'number')  throw ('a valid `year` argument is required');
+  if (typeof(month) !== 'number') throw ('a valid `month` argument is required');
+  if (typeof(day) !== 'number')   throw ('a valid `day` argument is required');
 
-  // Summary:
-  //    Localization, they shold store all language properties, the user may change the outoput names
-  //    if necessary using the localization argument.
+  // Assign a default localization dictionary.
   this.localization = {
         culture             : 'En-Us'
       , monthNames          : ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
@@ -30,30 +23,22 @@ var Calendar = function (year, month, day) {
       , MonthDayFormatEmpty : '----'
   };
 
-  // Summary:
-  //    Application variables, all other configuarion properties.
   this.monthDays       = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   this.weekDays        = 7;
-  this.days            = month == 2 && year % 4 == 0 ? 29 : this.monthDays[month - 1]; //Leap year bugfix
+  this.days            = month == 2 && year % 4 == 0 ? 29 : this.monthDays[month - 1]; // Leap year bugfix
   this.date            = new Date(year, month - 1, day);
   this.firstDayOfMonth = new Date(year, month - 1, 1);
   this.firstDayWeek    = this.firstDayOfMonth.getDay(); // Store the first day of the week (to avoid function loop call)
 
-  // Summary:
-  //    helpers and utilities, a colections of functions to improve lecture and productvity
   this.replace         = function(target, args) {
-
-    if (args ==  null || args.length <= 0) return;
+    if (args == null || args.length <= 0) return;
     for (var i = 0; i < args.length; ++i) target = target.replace(('{' + i + '}'), args[i])
     return target;
-
   };
 
 };
 
-// Summary:
-//    Prototypes, some functions of the object
-// REMARKS: WANT TO BE REFACTORED.
+// Generate and print the calendar.
 Calendar.prototype.show = function () {
 
   var dayBuffer;
@@ -61,26 +46,25 @@ Calendar.prototype.show = function () {
   var dayNum;
   var headerColumns = '';
   var dayCount = 0;
+  var isBufferFirstLine;
 
-  // Travelling between all week days
   for (var i = 0; i < this.weekDays; ++i) {
 
-    // Clear log buffer
     logBuffer = '';
 
-    // Construct day log
     for (var j = 0; j < this.weekDays; ++j) {
 
-        // Construct header if is the first week
-        if (i === 0) {
-            headerColumns += this.replace(this.localization.WeekDayFormat, [this.localization.weekDaysNames[j]]).white;
+        isBufferFirstLine = i === 0;
+
+        if (isBufferFirstLine) {
+					// Add the week day header. 
+					headerColumns += this.replace(this.localization.WeekDayFormat, [this.localization.weekDaysNames[j]]).white;
         }
 
-        // Set the day number
         dayCount = ((this.weekDays * i) + j) + 1;
         dayNum = dayCount - this.firstDayWeek;
 
-        // Write the log with 2 digits
+        // Validate and format the day string.
         if (dayNum > this.days) {
             continue;
         } else if (dayNum.toString().length >= 2) {
@@ -98,68 +82,51 @@ Calendar.prototype.show = function () {
         logBuffer += dayBuffer;
     }
 
-    // Print the header if is the first week
-    if (i == 0) {
+    // Print the header. 
+    if (isBufferFirstLine) {
         console.log(headerColumns.bgBlue);
     }
 
-    // Print the log day
+    // Print the body.
     console.log(logBuffer);
   }
 
 };
 
-// Summary:
-//   Get the current date from the calendar.
-// Return:
-//    Date : returns the the specfied calendars date.
+// Return the calendar's date.
 Calendar.prototype.getDate = function () {
-
   return this.date;
-
 };
 
-// Summary:
-//   Set's the specfied to the calendar, if the calendar is already displayCalendar
-//   the method show needs to be called against.
+// Set the calendar's date 
 // Arguments:
-//   - year(required): number of the specifed year.
-//   - month(required): number of the specified month, between(1 - 12).
-//   - day(required): number of day in the month, between(1 - 31).
+//   - year(required): number of the year.
+//   - month(required): number of the month, between 1 and 12.
+//   - day(required): number of day, between 1 and 31.
 Calendar.prototype.setDate = function (year, month, day) {
 
-  if (typeof(year) !== 'number')  throw ('an valid `year` argument is required');
-  if (typeof(month) !== 'number') throw ('an valid `month` argument is required');
-  if (typeof(day) !== 'number')   throw ('an valid `day` argument is required');
+  if (typeof(year) !== 'number')  throw ('a valid `year` argument is required');
+  if (typeof(month) !== 'number') throw ('a valid `month` argument is required');
+  if (typeof(day) !== 'number')   throw ('a valid `day` argument is required');
 
-  this.days            = month == 2 && year % 4 == 0 ? 29 : this.monthDays[month - 1]; //Leap year bugfix
+  this.days            = month == 2 && year % 4 == 0 ? 29 : this.monthDays[month - 1]; // Leap year bugfix
   this.date            = new Date(year, month - 1, day);
   this.firstDayOfMonth = new Date(year, month - 1, 1);
   this.firstDayWeek    = this.firstDayOfMonth.getDay();
 
 };
 
-// Summary:
-//    Update the current localization to the specifed language.
+// Update calendar's localization dictionary.
 // Arguments:
-//    - localization(required): the specfied culture location information.
+//   - localization(required): the localization dictionary.
 Calendar.prototype.setLocalization = function (localization) {
-
-    if (typeof(localization) !== 'object') throw ('an valid `localization` argument is required');
+    if (typeof(localization) !== 'object') throw ('a valid `localization` argument is required');
     this.localization = optionExtender(this.localization, localization);
-
 };
 
-// Summary:
-//    Update the current localization to the specifed language.
-// Returns:
-//    Returns the current localizatin configuration.
+// Return the calendar's localization dictionary.
 Calendar.prototype.getLocalization = function () {
-
     return this.localization;
-
 };
 
-
-// Exporting the calendar
 module.exports = Calendar;
